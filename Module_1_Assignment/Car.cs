642ceb7986788aeb012c2386c6d45622191ff46f
Mostofa <mostofacba@gmail.com>
2025-08-24 12:42:21 +0600

    Add project files.

---
+++ Module_1_Assignment/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Module_1_Assignment
+{
+    //Task 1: Create a Car Class
+    internal class Car
+    {
+        //Step 1. Create properties of Car Class:
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        //Step 2. Add a method called Drive
+        public void Drive(int kilometers)
+        {
+            Mileage += kilometers;
+            Console.WriteLine($"Car {Brand} {Model} has driven {kilometers} km. Total mileage: {Mileage} km.");
+        }
+
+        // Task 3: Add the ShowCarInfo method
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Car Info: Brand - {Brand}, Model - {Model}, Year - {Year}, Mileage - {Mileage}");
+        }    
+
+    }
+}
---
+++ Module_1_Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace Module_1_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2: Create Objects of the Class
+            //Create two different Car objects(e.g., Toyota, Tesla).
+
+            Car toyotaCorolla = new Car
+            {
+                Brand = "Toyota",
+                Model = "Corolla",
+                Year = 2020,
+                Mileage = 0
+            };
+            Car teslaModel3 = new Car
+            {
+                Brand = "Tesla",
+                Model = "Model 3",
+                Year = 2021,
+                Mileage = 0
+            };
+
+            ///Step 2. Call the Drive method on both cars(different kilometers for each).Expected Output(Example):
+            toyotaCorolla.Drive(50); // Car Toyota Corolla has driven 50 km. Total mileage: 50 km.
+            teslaModel3.Drive(120); // Car Tesla Model 3 has driven 120 km. Total mileage: 120 km.
+            toyotaCorolla.Drive(30); // Car Toyota Corolla has driven 30 km. Total mileage: 80 km.
+
+
+            // Now we can call the new ShowCarInfo() method on each object
+            Console.WriteLine("\nDisplaying final car information:");
+            toyotaCorolla.ShowCarInfo(); 
+            teslaModel3.ShowCarInfo();   
+
+
+        }
+    }
+}
